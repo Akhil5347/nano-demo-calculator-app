a26282186e02d5c3ab1415870800e8760d030948
Akhil <mgakhil03@gmail.com>
2023-08-06 19:48:42 +0530

    Changes 2

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return (render_template('index.html'))
+    return "Hello World!"
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
@@ -16,22 +16,21 @@ def add():
         
         result = num1 + num2
         
-        return jsonify({" result": result})
+        return jsonify({"result": result})
     except KeyError:
         return jsonify({"error": "Invalid input"}), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    try:
-        data = request.json
-        num1 = data['first']
-        num2 = data['second']
+    
+    data = request.json
+    num1 = data['first']
+    num2 = data['second']
         
-        result = num1 - num2
+    result = num1 - num2
         
-        return jsonify({" result": result})
-    except KeyError:
-        return jsonify({"error": "Invalid input"}), 400
+    return jsonify({"result": result})
+    
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
